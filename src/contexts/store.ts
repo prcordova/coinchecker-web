f53d5824a20e2d24f0183e9a57967df8ecc4cf33
prcordova <pedroricardocordova@yahoo.com.br>
2024-06-20 03:33:56 -0300

    feat: zustand now persists states

--- src/contexts/store.ts
+++ src/contexts/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface StoreState {
   isPremium: boolean;
@@ -7,11 +8,18 @@ interface StoreState {
   setSubscriptionId: (id: string | null) => void;
 }
 
-const useStore = create<StoreState>((set) => ({
-  isPremium: false,
-  setIsPremium: (status: boolean) => set({ isPremium: status }),
-  subscriptionId: null,
-  setSubscriptionId: (id: string | null) => set({ subscriptionId: id }),
-}));
+const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      isPremium: false,
+      setIsPremium: (status: boolean) => set({ isPremium: status }),
+      subscriptionId: null,
+      setSubscriptionId: (id: string | null) => set({ subscriptionId: id }),
+    }),
+    {
+      name: "user-subscription-storage", // Nome do item no localStorage
+    }
+  )
+);
 
 export default useStore;
